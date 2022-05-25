a8b244c88048996095a97d676dbdf478e2ffba44
hsinyichang <f1233772002@gmail.com>
2022-05-25 08:15:11 +0800

    html課程

---
+++ h0519function/main.js
@@ -0,0 +1,74 @@
+// let result=document.getElementById("result");
+//抓元件  文字內容改掉
+//let宣告變數 可以變化
+//const 宣告變數 不可變化
+//constants(常數)類似使用let宣告，具有區塊可視範圍，不能重複指定也不能重複宣告。
+// result.innerText="hello first change text";
+//改元件內容文字
+
+const resultP=document.getElementById("result");//宣告變數從文件
+const num1Input=document.getElementById("num1");
+const num2Input=document.getElementById("num2");
+const myform=document.getElementById("myform");
+const optionInput=document.getElementById('option');
+resultP.innerText="hello first change text";
+
+
+
+console.log(resultP);
+console.log(num1Input);
+console.log(num2Input);
+console.log(myform);
+
+
+// 當這個myform表單內的submit送出
+// 練習一   抓到num1  num2的value
+
+myform.addEventListener('submit',function(e){//加入事件(按下submit後所會執行function內容)
+    e.preventDefault();//submit不送出表單
+    console.log(e);
+    console.log(num1Input.value);
+    console.log(num2Input.value);
+    let num1=parseInt(num1Input.value);
+    let num2=parseInt(num2Input.value);
+    let option=optionInput.value;
+
+    let resultNum=0;//先宣告結果為0
+    switch (option) {
+        case "+":
+            resultNum = num1 + num2 ;        
+            break;
+        case "-":
+            resultNum = num1 - num2 ;        
+            break;
+        case "*":
+            resultNum = num1 * num2 ;        
+            break;
+        case "/":
+            resultNum = num1 / num2 ;        
+            break;
+    
+        default:
+            break;
+    }
+
+    console.log(resultNum);
+    mystring1=(`${num1}${option}${num2}=${resultNum}`);
+    // ``這個符號在esc下面
+    
+    resultP.innerText=mystring1;
+    //submit get value
+})
+
+// 練習二
+// num1 111
+// num2 222
+// option +
+// result 111 + 222 = 333
+// + - * /
+// + - * / 顯示結果出來
+
+// 練習三
+// option input text -> select
+
+
---
+++ h0519function/order.js
@@ -0,0 +1,37 @@
+const item1 = document.getElementById('item1');
+const item1Num = document.getElementById('item1Num');
+const item1Price = document.getElementById('item1Price');
+
+const item2 = document.getElementById('item2');
+const item2Num = document.getElementById('item2Num');
+const item2Price = document.getElementById('item2Price');
+
+// button 1.click / button submit ->form
+const myForm = document.getElementById('myForm');
+// const myBtn = document.getElementById('myBtn');
+//以上先宣告變數  抓資料
+
+
+//抓完資料後設定button觸發事件
+myForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    let item1Val = parseInt(item1.value);
+    let item1NumVal = parseInt(item1Num.value);
+    let item1Result = item1Val * item1NumVal ;//總價=單價*數量
+    item1Price.value = item1Result;
+    // console.log('item1Val',item1Val);
+
+    let item2Val = parseInt(item2.value);
+    let item2NumVal = parseInt(item2Num.value);
+    let item2Result = item2Val * item2NumVal ;//總價=單價*數量
+    item2Price.value = item2Result;
+
+    let item3Val = parseInt(item3.value);
+    let item3NumVal = parseInt(item3Num.value);
+    let item3Result = item3Val * item3NumVal ;//總價=單價*數量
+    item3Price.value = item3Result;
+    sum=item1Result+item2Result+item3Result;//總價
+
+    result.innerText=`${sum}`;//最後總價
+}
+)
